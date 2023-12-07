380c26916dd061c6ccbc35856297187d71e6d0a3
Oleh Kot <88795955+OlehKotynskyi@users.noreply.github.com>
2023-12-07 08:54:01 +0200

    amendments-2

--- src/add-js/pixabayAPI.js
+++ src/add-js/pixabayAPI.js
@@ -24,19 +24,4 @@ export async function gettingApiImages(query, page) {
       Notiflix.Notify.failure('Sorry, there are problems with API.');
       throw new Error(error);
    }
-}
-
-
-export function smoothScroll(page) {
-   if (page === 1) {
-      return;
-   }
-
-   const galleryChildsList = document.querySelectorAll('.photo-card');
-   const { height: cardHeight } = galleryChildsList[galleryChildsList.length - 1].getBoundingClientRect();
-
-   window.scrollBy({
-      top: cardHeight * 2,
-      behavior: "smooth",
-   });
-}
+};
